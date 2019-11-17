ea088bbe06d1d3d93a33bde02a1cb3847ee049bf
srujanm111 <srujanmup@gmail.com>
2019-11-17 10:49:12 -0600

    Added Skeleton to test out

---
+++ src/Utilities/Skeleton.java
@@ -0,0 +1,12 @@
+package Utilities;
+
+import java.util.Scanner;
+
+public class Skeleton {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
